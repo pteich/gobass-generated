d646e5a394defdbe1a9920fbb0f1f63079e714fd
Peter Teich <peter.teich@gmail.com>
2022-08-01 13:27:03 +0200

    Add example

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Generated using https://github.com/xlab/c-for-go
 
 For more information on Un4seen BASS and licensing information see https://www.un4seen.com/
+
+# Example
+
+A basic working example can be found in main.go. It is missing real stream URLs but you probably get
+the general usage.
--- main.go
+++ main.go
@@ -1,22 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
 	"unsafe"
 
 	"github.com/pteich/gobass-generated/bass"
 )
 
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGKILL)
+	defer cancel()
+
 	win := 0
 	dsguid := 0
-	bass.BassInit(-1, 44100, 0, unsafe.Pointer(&win), unsafe.Pointer(&dsguid))
-
-	hstream := bass.BassStreamcreateurl("https://stream.sunshine-live.de/sp8/mp3-192", 0, 0, nil, nil)
-	if hstream == 0 {
-		errCode := bass.BassErrorgetcode()
-		fmt.Println(errCode)
-		os.Exit(1)
+	ret := bass.BassInit(0, 44100, bass.BassDeviceNospeaker|bass.BassDeviceStereo, unsafe.Pointer(&win), unsafe.Pointer(&dsguid))
+	if ret <= 0 {
+		log.Fatal(bass.BassErrorgetcode())
 	}
+
+	defer bass.BassFree()
+
+	hstream := bass.BassStreamcreateurl("Http://url-of-a-icecast-stream", 0, bass.BassStreamAutofree, nil, nil)
+	if hstream == 0 {
+		log.Fatal(bass.BassErrorgetcode())
+	}
+
+	var syncProc bass.Syncproc = func(handle uint32, channel uint32, data uint32, user unsafe.Pointer) {
+		log.Print(bass.BassChannelgettags(channel, bass.BassTagMeta))
+	}
+
+	hsync := bass.BassChannelsetsync(hstream, bass.BassSyncMeta, 0, syncProc, nil)
+	if hsync == 0 {
+		log.Fatal(bass.BassErrorgetcode())
+	}
+	defer bass.BassStreamfree(hstream)
+
+	hencode := bass.BassEncodeMp3Start(hstream, "-r -s 44100 -b 128", bass.BassEncodeAutofree, nil, nil)
+	if hencode == 0 {
+		log.Fatal(bass.BassErrorgetcode())
+	}
+
+	ret = bass.BassChannelplay(hstream, 1)
+	if ret <= 0 {
+		log.Fatal(bass.BassErrorgetcode())
+	}
+
+	ret = bass.BassEncodeCastinit(hencode,
+		"icecast-server:8000/mount",
+		"password",
+		bass.BassEncodeTypeMp3,
+		"Bass Test",
+		"url",
+		"",
+		"",
+		"",
+		128,
+		bass.BassEncodeCastPublic,
+	)
+	if ret <= 0 {
+		log.Fatal(bass.BassErrorgetcode())
+	}
+
+	<-ctx.Done()
 }
