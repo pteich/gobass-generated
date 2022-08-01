9686fe73f1a8807215ee3c23e24fd36dc54c2c62
Peter Teich <peter.teich@gmail.com>
2022-08-01 08:59:04 +0200

    Change import path

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"unsafe"
 
-	"github.com/pteich/gobass/bass"
+	"github.com/pteich/gobass-generated/bass"
 )
 
-func main()  {
+func main() {
 	win := 0
 	dsguid := 0
 	bass.BassInit(-1, 44100, 0, unsafe.Pointer(&win), unsafe.Pointer(&dsguid))
+
+	hstream := bass.BassStreamcreateurl("https://stream.sunshine-live.de/sp8/mp3-192", 0, 0, nil, nil)
+	if hstream == 0 {
+		errCode := bass.BassErrorgetcode()
+		fmt.Println(errCode)
+		os.Exit(1)
+	}
 }
