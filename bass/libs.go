8dbe6a7a88699cc1d4d2d1d77621149089fee715
Peter Teich <peter.teich@gmail.com>
2022-08-01 10:58:57 +0200

    Upgrade mix lib

--- bass/libs.go
+++ bass/libs.go
@@ -1,12 +1,12 @@
 package bass
 
 /*
-#cgo linux,386 LDFLAGS: -Wl,-rpath=${SRCDIR}/../lib/linux_386 -L${SRCDIR}/lib/linux_386 -Wl,-Rpath=${SRCDIR}/lib/linux_386
-#cgo linux,amd64 LDFLAGS: -Wl,-rpath=${SRCDIR}/../lib/linux_amd64 -L${SRCDIR}/lib/linux_amd64
+#cgo linux,386 LDFLAGS: -Wl,-rpath=${SRCDIR}/../lib/linux_386 -L${SRCDIR}/../lib/linux_386 -Wl,-Rpath=${SRCDIR}/../lib/linux_386
+#cgo linux,amd64 LDFLAGS: -Wl,-rpath=${SRCDIR}/../lib/linux_amd64 -L${SRCDIR}/../lib/linux_amd64
 #cgo windows,386 LDFLAGS: -L${SRCDIR}/../lib/windows_386
 #cgo windows,amd64 LDFLAGS: -L${SRCDIR}/../lib/windows_amd64
 #cgo darwin LDFLAGS: -L${SRCDIR}/../lib/darwin
-#cgo LDFLAGS: -lbass -lbassenc -lbassenc_mp3 -lbassenc_flac -lbassenc_ogg -lbassmix
+#cgo LDFLAGS: -lbass -lbassenc -lbassenc_mp3 -lbassenc_flac -lbassenc_ogg -lbassenc_aac -lbassmix
 
 #cgo CFLAGS: -I${SRCDIR}/../include
 */
